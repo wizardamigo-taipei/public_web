213a3f74039284305f7cb51a607f771f903d1879
yuanyu90221 <json@cbx.io>
2019-06-18 10:52:17 +0800

    add title

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-// import Head from 'next/head';
+import Head from 'next/head';
 import {Button, Card, Row} from 'antd'
 const Home = () => (
   <Card>
+    <Head>
+      <title>WizardAmigo Taipei</title>
+    </Head>
     <Row justify="center" type="flex">Welcome to Wizard Amigos Taipei</Row>
     <Row justify="center" type="flex"><Button type="primary">Button</Button></Row>
   </Card>
